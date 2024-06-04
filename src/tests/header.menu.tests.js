676d63da36a0be76d630677549ad288a9ec828fd
illbetrue <47645739+illbetrue@users.noreply.github.com>
2024-06-04 14:13:56 +0300

    Update header.menu.tests.js

--- src/tests/header.menu.tests.js
+++ src/tests/header.menu.tests.js
@@ -13,9 +13,9 @@ describe('Tests suit', async () => {
   await basePage.closeCookiesBar();
   });
 
-  /*it('Get title', async () => {
+  /it('Get title', async () => {
       await expect(browser).toHaveTitle('EPAM | Software Engineering & Product Development Services');
-  });*/
+  });
 
   it('Hover the specified category in the header Menu', async () => {
     
